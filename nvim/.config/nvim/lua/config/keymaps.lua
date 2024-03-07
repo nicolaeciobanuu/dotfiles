d7c56b0b1685a0fbc995d09162cd28d253368c42
ciobanunicolae <ciobanu.nicolae.j@gmail.com>
2024-03-07 18:19:41 +0100

    nvim: change leader to ','

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,17 @@
 local function map(mode, lhs, rhs, opts)
-   if opts.noremap == nil then
-      opts.noremap = true
-   end
-   if opts.silent == nil then
-      opts.silent = true
-   end
+	if opts.noremap == nil then
+		opts.noremap = true
+	end
+	if opts.silent == nil then
+		opts.silent = true
+	end
 
-   vim.keymap.set(mode, lhs, rhs, opts)
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 -- leader
-vim.g.mapleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- window movements
 map("n", "<C-k>", "<C-w>k", {})
